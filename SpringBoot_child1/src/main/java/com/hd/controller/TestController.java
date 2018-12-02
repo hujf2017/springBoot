8cad71de8b8345c36bb31d289f43286d27c15c93
hujf <952405831@qq.com>
2018-12-02 15:46:53 +0800

    日志管理

---
+++ SpringBoot_child1/src/main/java/com/hd/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.hd.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	private Logger logger	=LoggerFactory.getLogger(this.getClass());
+	
+	@RequestMapping("/show") 
+	@ResponseBody
+	public String show(){
+		
+		logger.debug("debug日志");
+		logger.info("info日志");
+		logger.warn("warn日志");
+		logger.error("error日志");
+		
+		return "show";
+		
+	}
+}
